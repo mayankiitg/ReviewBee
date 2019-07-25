8de1dad813b9c1fdbf5a4d3ece1b78743ae03eb7
chandananjani2 <53098530+chandananjani2@users.noreply.github.com>
2019-07-24 19:47:36 -0700

    Update Startup.cs

--- Application/aspnet-core-dotnet-core/Startup.cs
+++ Application/aspnet-core-dotnet-core/Startup.cs
@@ -23,7 +23,8 @@ namespace aspnet_core_dotnet_core
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Again dummy comment.
+            
+            
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
