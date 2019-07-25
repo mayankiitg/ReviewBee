c332934a0486ea83aaed1bb8e220f3080ab413c8
chandananjani2 <53098530+chandananjani2@users.noreply.github.com>
2019-07-24 19:48:02 -0700

    Update Startup.cs

--- Application/aspnet-core-dotnet-core/Startup.cs
+++ Application/aspnet-core-dotnet-core/Startup.cs
@@ -18,6 +18,7 @@ namespace aspnet_core_dotnet_core
             Configuration = configuration;
         }
 
+        
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
