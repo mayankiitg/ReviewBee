67806efa46ba8c03bbd51ca5e52a7fcb42509280
chandananjani2 <53098530+chandananjani2@users.noreply.github.com>
2019-07-24 19:35:39 -0700

    Update CartService.cs

--- Application/aspnet-core-dotnet-core/CartService.cs
+++ Application/aspnet-core-dotnet-core/CartService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace aspnet_core_dotnet_core
 {
     
-    //sample demo
+    //sample demo1
     public class CartService
     {
         public void AddItemToCart()
